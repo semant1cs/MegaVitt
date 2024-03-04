d924ffce22eb3460c193af009f178f45bca9cc08
Mikhail Petrov <59141793+semant1cs@users.noreply.github.com>
2024-03-04 15:33:25 +0500

    добавляет сваггер

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mega Vitt API')
+    .setDescription('API для веб-сервиса по созданию мероприятий')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
