d6de05d714b4cc84800864cbcc8a7918d43d9409
Mikhail Petrov <59141793+semant1cs@users.noreply.github.com>
2024-03-05 15:23:34 +0500

    добавляет модель сущности билетов

--- backend/src/ticket/entities/ticket.entity.ts
+++ backend/src/ticket/entities/ticket.entity.ts
@@ -1,1 +1,44 @@
-export class Ticket {}
+import { ApiProperty } from '@nestjs/swagger';
+import { UUIDV4 } from 'sequelize';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface TicketCreationAttributes {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'ticket' })
+export default class Ticket extends Model<Ticket, TicketCreationAttributes> {
+  @Column({
+    type: DataType.UUIDV4,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: UUIDV4,
+  })
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'Уникальный ID билета',
+  })
+  id: string;
+
+  @Column({
+    type: DataType.UUIDV4,
+    defaultValue: UUIDV4,
+  })
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'ID владельца билета',
+  })
+  owner_id: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+  })
+  @ApiProperty({
+    example: 400,
+    description: 'Стоимость билета',
+  })
+  cost: number;
+}
--- backend/src/ticket/ticket.module.ts
+++ backend/src/ticket/ticket.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import Ticket from './entities/ticket.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Ticket])],
   controllers: [TicketController],
   providers: [TicketService],
 })
