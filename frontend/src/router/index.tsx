048d701d22c273470a5f035411fe22337767c2b2
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-21 02:03:47 +0500

    чинит сборку фронта v1

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,60 +1,51 @@
-import React, {ReactNode} from 'react';
-import {Routes, Route, useLocation, Navigate} from "react-router-dom";
+import React, { ReactNode } from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Welcome from "@components/Welcome/Welcome.container";
 
 type RouteType = {
-    path: string,
-    element: ReactNode,
-}
+  path: string;
+  element: ReactNode;
+};
 
 const AppRouter: React.FC = () => {
-    const publicRoutes: RouteType[] = [
-        {
-            path: "/",
-            element: <Welcome/>
-        },
-    ]
+  const publicRoutes: RouteType[] = [
+    {
+      path: "/",
+      element: <Welcome />,
+    },
+  ];
 
-    const privateRoutes: RouteType[] = [
-        {
-            path: "/",
-            element: <Welcome/>
-        },
-    ]
+  const privateRoutes: RouteType[] = [
+    {
+      path: "/",
+      element: <Welcome />,
+    },
+  ];
 
-    const location = useLocation()
-    const isAuth = true
+  const location = useLocation();
+  const isAuth = true;
 
-    return (
-        isAuth ?
-            (
-                privateRoutes.findIndex(comp => comp.path === location.pathname) !== -1
-                    ?
-                    <Routes>
-                        {
-                            privateRoutes.map(({path, element}, index) =>
-                                <Route path={path} element={element} key={index}/>
-                            )
-                        }
-                    </Routes>
-                    :
-                    <Navigate to="/personalArea"/>
-
-            ) :
-            (
-                publicRoutes.findIndex(comp => comp.path === location.pathname) !== -1
-                    ?
-                    <Routes>
-                        {
-                            publicRoutes.map(({path, element}, index) =>
-                                <Route path={path} element={element} key={index}/>
-                            )
-                        }
-                    </Routes>
-                    :
-                    <Navigate to="/"/>
-            )
-    );
+  return isAuth ? (
+    privateRoutes.findIndex((comp) => comp.path === location.pathname) !==
+    -1 ? (
+      <Routes>
+        {privateRoutes.map(({ path, element }, index) => (
+          <Route path={path} element={element} key={index} />
+        ))}
+      </Routes>
+    ) : (
+      <Navigate to="/personalArea" />
+    )
+  ) : publicRoutes.findIndex((comp) => comp.path === location.pathname) !==
+    -1 ? (
+    <Routes>
+      {publicRoutes.map(({ path, element }, index) => (
+        <Route path={path} element={element} key={index} />
+      ))}
+    </Routes>
+  ) : (
+    <Navigate to="/" />
+  );
 };
 
 export default AppRouter;
