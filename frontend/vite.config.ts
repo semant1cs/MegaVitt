b3d0f785bd2335e37be00ccd83bf67b67c610ae9
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-21 02:08:55 +0500

    починил билд, теперь нормально реагирует на алиасы

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,19 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            components: "/src/components",
-            store: "/src/store",
-            styles: "/src/assets/globalStyles"
-        }
-    }
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      store: "/src/store",
+      styles: "/src/assets/globalStyles",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["@components/Welcome/Welcome.container"],
+    },
+  },
+});
