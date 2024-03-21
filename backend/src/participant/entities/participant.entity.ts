0be286c007149f42ae49317ef12b2cb68a275481
donesait <yagufarove@gmail.com>
2024-03-21 18:08:51 +0500

    Описал модель participant, добавил participant dto

--- backend/src/participant/entities/participant.entity.ts
+++ backend/src/participant/entities/participant.entity.ts
@@ -1,1 +1,43 @@
-export class Participant {}
+import { UUIDV4 } from 'sequelize';
+import { DataType, Column, Model, Table, BelongsTo, ForeignKey } from 'sequelize-typescript';
+import Event from '../../event/entities/event.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+interface ParticipantCreationAttributes {
+  name: string;
+}
+
+@Table({ tableName: 'participant' })
+export default class Participant extends Model<Participant, ParticipantCreationAttributes> {
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: UUIDV4,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: UUIDV4,
+  })
+
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'Уникальный айди пользователя',
+  })
+  @ForeignKey(()=> Event)
+  @Column({
+    type: DataType.STRING,
+    unique: true
+  })
+  user_id: string;
+
+  @BelongsTo(() => Event)
+  event: Event;
+}
+
