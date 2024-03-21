c4c0bcfcd5ce6b8f46088d6d378214c57e8e88bb
donesait <yagufarove@gmail.com>
2024-03-21 13:56:42 +0500

    описал модель event

--- backend/src/event/entities/event.entity.ts
+++ backend/src/event/entities/event.entity.ts
@@ -1,11 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { UUIDV4 } from 'sequelize';
-import { DataType, Column, Model } from 'sequelize-typescript';
+import {ApiProperty} from '@nestjs/swagger';
+import {UUIDV4} from 'sequelize';
+import {DataType, Column, Model, Table} from 'sequelize-typescript';
 
 interface EventCreationAttributes {
   name: string;
 }
 
+@Table({ tableName: 'event' })
 export default class Event extends Model<Event, EventCreationAttributes> {
   @Column({
     type: DataType.STRING,
@@ -17,47 +18,97 @@ export default class Event extends Model<Event, EventCreationAttributes> {
   id: string;
 
   @Column({
-    type: DataType.STRING,
-    unique: true,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: UUIDV4,
+    type: DataType.BOOLEAN,
+    defaultValue: false
   })
-  name: string;
+  @ApiProperty({
+    example: 'true',
+    description: 'Участники обязательны или нет'
+  })
+  is_participant_required: boolean;
 
   @Column({
-    type: DataType.STRING,
-    unique: true,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: UUIDV4,
+    type: DataType.INTEGER,
+    allowNull: true
   })
-  build_ref: string;
+  @ApiProperty({
+    example: '123',
+    description: 'Количество участников'
+  })
+  participant_count: number;
 
   @Column({
-    type: DataType.STRING,
-    unique: true,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: UUIDV4,
+    type: DataType.INTEGER,
+    allowNull: true
   })
-  link: string;
+  @ApiProperty({
+    example: '12345',
+    description: 'Лимит участников на событии'
+  })
+  participant_limit: number;
 
   @Column({
-    type: DataType.STRING,
-    unique: true,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: UUIDV4,
+    type: DataType.BOOLEAN,
+    defaultValue: false
   })
-  owner_id: string;
+  @ApiProperty({
+    example: 'true',
+    description: 'Билеты обязательны или нет'
+  })
+  is_tickets_required: boolean;
 
   @Column({
-    type: DataType.STRING,
-    unique: true,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: UUIDV4,
+    type: DataType.INTEGER,
+    allowNull: true
   })
-  event: string;
+  @ApiProperty({
+    example: '12345',
+    description: 'Цена билета'
+  })
+  ticket_cost: number;
+
+  // @Column({
+  //   type: DataType.STRING,
+  //   unique: true,
+  //   primaryKey: true,
+  //   allowNull: false,
+  //   defaultValue: UUIDV4,
+  // })
+  // name: string;
+  //
+  // @Column({
+  //   type: DataType.STRING,
+  //   unique: true,
+  //   primaryKey: true,
+  //   allowNull: false,
+  //   defaultValue: UUIDV4,
+  // })
+  // build_ref: string;
+  //
+  // @Column({
+  //   type: DataType.STRING,
+  //   unique: true,
+  //   primaryKey: true,
+  //   allowNull: false,
+  //   defaultValue: UUIDV4,
+  // })
+  // link: string;
+  //
+  // @Column({
+  //   type: DataType.STRING,
+  //   unique: true,
+  //   primaryKey: true,
+  //   allowNull: false,
+  //   defaultValue: UUIDV4,
+  // })
+  // owner_id: string;
+  //
+  // @Column({
+  //   type: DataType.STRING,
+  //   unique: true,
+  //   primaryKey: true,
+  //   allowNull: false,
+  //   defaultValue: UUIDV4,
+  // })
+  // event: string;
 }
