9f3fa9cc15789cc07914dcc46ad8c150d6eb6b7a
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-22 19:34:26 +0500

    добавляет декоратор для защиты эндпоинтов

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -3,12 +3,14 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import User from './entities/user.entity';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
   @Post()
+  @Public()
   createUser(@Body() dto: CreateUserDto) {
     return this.userService.create(dto);
   }
