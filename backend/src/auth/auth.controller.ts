fd78d6b638f7867f9faa914a378c405d6a00f399
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-22 19:35:39 +0500

    добавляет эндпоинты для авторизации и получение авторизованного пользователя

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from '@nestjs/common';
+import User from 'src/user/entities/user.entity';
+import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
+import { Controller, HttpStatus, Post, HttpCode, Body, Get, Request } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req): Promise<User> {
+    return req.user;
+  }
+}
