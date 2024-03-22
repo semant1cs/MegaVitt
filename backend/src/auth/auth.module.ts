888f30a80746baa6e6dd9c8bb5fdfd458eb8b415
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-22 19:37:09 +0500

    подгружает все составляющие в модуле и константа

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,31 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { AuthGuard } from './auth.guard';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
-  imports: [forwardRef(() => UserModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    forwardRef(() => PassportModule),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
