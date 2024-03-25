5f6a01ee6cd7c806aeba0065f05f061dc41bb20c
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-25 13:26:43 +0500

    дорабатывает dto для регистрации, +эндпоинт к ней

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,7 +3,8 @@ import { AuthService } from './auth.service';
 import { Public } from './decorators/public.decorator';
 import { Controller, HttpStatus, Post, HttpCode, Body, Get, Request } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { LogInDto } from './dto/sign-in.dto';
+import { LoginDto } from './dto/login.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -11,13 +12,21 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  @ApiBearerAuth()
   @HttpCode(HttpStatus.OK)
-  signIn(@Body() LogInDto: LogInDto) {
-    return this.authService.signIn(LogInDto.email, LogInDto.password);
+  @Public()
+  login(@Body() logInDto: LoginDto) {
+    return this.authService.Login(logInDto);
+  }
+
+  @Post('signIn')
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  signIn(@Body() SignInDto: SignInDto) {
+    return this.authService.SignIn(SignInDto);
   }
 
   @Get('profile')
+  @ApiBearerAuth()
   getProfile(@Request() req): Promise<User> {
     return req.user;
   }
--- backend/src/auth/dto/sign-in.dto.ts
+++ backend/src/auth/dto/sign-in.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class LogInDto {
+export class SignInDto {
   @ApiProperty({ example: 'admin@example.com', description: 'Email пользователя' })
   readonly email: string;
+
+  @ApiProperty({ example: 'admin@example.com', description: 'Email пользователя' })
+  readonly username: string;
 
   @ApiProperty({ example: '12345678', description: 'admin@example.com' })
   readonly password: string;
