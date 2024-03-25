1368aa255d658a9df2a8c204e2d4bd1aa40a7a13
Petrov Mikhail <mihail323i21@gmail.com>
2024-03-25 13:27:49 +0500

    добавляет логику работу регистрации

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,9 @@
+import { LoginDto } from './dto/login.dto';
+import { SignInDto } from './dto/sign-in.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from './../user/user.service';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import User from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -9,15 +12,35 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async signIn(email: string, pass: string) {
+  async Login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
     const user = await this.userService.findOneByEmail(email);
 
-    if (user?.password !== pass) {
+    if (user?.password !== password) {
       throw new UnauthorizedException();
     }
     const payload = { username: user.email, sub: user.id };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async SignIn(signInDto: SignInDto) {
+    const { email, password, username } = signInDto;
+
+    if (await this.IsUserExists(email)) {
+      console.log(1);
+      throw new HttpException('Пользователь с такой почтой уже существует', 400);
+    } else {
+      console.log(2);
+      await this.userService.create({ email: email, password: password, username: username });
+      return this.Login({ email: email, password: password });
+    }
+  }
+
+  private async IsUserExists(email: string) {
+    const user = await this.userService.findOneByEmail(email);
+
+    return user !== undefined;
   }
 }
--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -4,6 +4,9 @@ export class CreateUserDto {
   @ApiProperty({ example: 'admin@example.com', description: 'Почта пользователя' })
   readonly email: string;
 
+  @ApiProperty({ example: 'examplenick', description: 'Никнейм пользователя' })
+  readonly username: string;
+
   @ApiProperty({ example: '12345678', description: 'Пароль пользователя' })
   readonly password: string;
 }
