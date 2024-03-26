c222b34051f4c7482be466df0f087c5e10b83975
donesait <yagufarove@gmail.com>
2024-03-26 19:24:35 +0500

    Добавил ForeignKey в site, связь с user

--- backend/src/site/site.module.ts
+++ backend/src/site/site.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SiteService } from './site.service';
 import { SiteController } from './site.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import Site from './entities/site.entity';
+import User from '../user/entities/user.entity';
 
 @Module({
   controllers: [SiteController],
+  imports: [SequelizeModule.forFeature([Site, User])],
   providers: [SiteService],
 })
 export class SiteModule {}
