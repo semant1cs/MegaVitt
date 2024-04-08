6faacd05d0322014f6f8a837c8e162c0523d4465
Petrov Mikhail <mihail323i21@gmail.com>
2024-04-08 18:35:21 +0500

    добавляет сущность роли и его DTO для создания и изменения

---
+++ backend/src/role/entities/user-role.entity.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { UUIDV4 } from 'sequelize';
+import { Column, DataType, Model, Table, ForeignKey } from 'sequelize-typescript';
+import Role from './role.entity';
+import User from 'src/user/entities/user.entity';
+
+@Table({ tableName: 'user-roles', createdAt: false, updatedAt: false })
+export default class UserRole extends Model<UserRole> {
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+  })
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'Уникальный айди',
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'ID роли принадлежащей пользователю',
+  })
+  @ForeignKey(() => Role)
+  roleId: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  @ApiProperty({
+    example: '488f36c1-d856-456a-b3bd-6bba5fba64f4',
+    description: 'ID пользователя',
+  })
+  @ForeignKey(() => User)
+  userId: string;
+}
