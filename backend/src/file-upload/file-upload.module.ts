fb0121e9c5a60997b0505fbfe13138489d50a213
Emil <yagufarove@gmail.com>
2024-04-15 16:26:01 +0500

    добавил в модулоь конфигурацю => DynamicAvatarDestinationMiddleware применяется только для file-upload/avatar

--- backend/src/file-upload/file-upload.module.ts
+++ backend/src/file-upload/file-upload.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
 import { FileUploadController } from './file-upload.controller';
+import { DynamicAvatarDestinationMiddleware } from './destination-middlewares/avatar-destination-middleware.service';
 
 @Module({
   controllers: [FileUploadController],
   providers: [FileUploadService],
 })
-export class FileUploadModule {}
+export class FileUploadModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(DynamicAvatarDestinationMiddleware).forRoutes('file-upload/avatar')
+    }
+}
