4ae4d5b7970c28f980bbdb8341b5d7ac399a7ad5
Petrov Mikhail <mihail323i21@gmail.com>
2024-04-15 17:10:34 +0500

    удаляет лишние эндпоинты и дто

--- backend/src/file-upload/file-upload.controller.ts
+++ backend/src/file-upload/file-upload.controller.ts
@@ -1,48 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
 import * as multer from 'multer';
 import { FileUploadService } from './file-upload.service';
-import { UpdateFileUploadDto } from './dto/update-file-upload.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-
 
 @Controller('file-upload')
 export class FileUploadController {
-  constructor(private readonly _fileUploadService: FileUploadService) {
-  }
+  constructor(private readonly _fileUploadService: FileUploadService) {}
 
   @Post('/avatar')
-  @UseInterceptors(FileInterceptor('file', {
-    storage: multer.diskStorage({
-      destination: (req: any, file: Express.Multer.File, cb): void => {
-        cb(null, req['dynamicDestination']);
-      },
-      filename: (req: any, file: Express.Multer.File, cb): void => {
-        const filename: string = file.originalname.replace(/^.*(?=\.[^.]+$)/, 'avatar');
-        cb(null, filename);
-      },
-    }),
-  }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: multer.diskStorage({
+        destination: (req: any, file: Express.Multer.File, cb): void => {
+          cb(null, req['dynamicDestination']);
+        },
+        filename: (req: any, file: Express.Multer.File, cb): void => {
+          const filename: string = file.originalname.replace(/^.*(?=\.[^.]+$)/, 'avatar');
+          cb(null, filename);
+        },
+      }),
+    })
+  )
   uploadAvatar() {
     return this._fileUploadService.uploadAvatar();
-  }
-
-  @Get()
-  findAll() {
-    return this._fileUploadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this._fileUploadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileUploadDto: UpdateFileUploadDto) {
-    return this._fileUploadService.update(+id, updateFileUploadDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this._fileUploadService.remove(+id);
   }
 }
--- backend/src/file-upload/file-upload.service.ts
+++ backend/src/file-upload/file-upload.service.ts
@@ -1,25 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateFileUploadDto } from './dto/update-file-upload.dto';
 
 @Injectable()
 export class FileUploadService {
   uploadAvatar(): string {
     return 'Аватар успешно загружен';
-  }
-
-  findAll() {
-    return `This action returns all fileUpload`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} fileUpload`;
-  }
-
-  update(id: number, updateFileUploadDto: UpdateFileUploadDto) {
-    return `This action updates a #${id} fileUpload`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} fileUpload`;
   }
 }
