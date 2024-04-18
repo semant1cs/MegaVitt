623ee9daacf3cb6c49ba238e2ae3db1e1b83b2b5
Petrov Mikhail <mihail323i21@gmail.com>
2024-04-19 00:14:24 +0500

    правильное использование env переменных

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ParticipantModule } from './participant/participant.module';
 import { EventModule } from './event/event.module';
@@ -22,18 +22,23 @@ import * as path from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: `.env` }),
-    SequelizeModule.forRoot({
-      retryAttempts: 3,
-      retryDelay: 3000,
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [Event, User, Ticket, Participant, UserRole, Role, Site],
-      autoLoadModels: true,
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: 3,
+        retryDelay: 3000,
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        models: [Event, User, Ticket, Participant, UserRole, Role, Site],
+        autoLoadModels: true,
+      }),
     }),
     ServeStaticModule.forRoot({ rootPath: path.join(__dirname, '..', '..', 'src', 'files') }),
     UserModule,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,20 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('APP_PORT') || 3001;
+  const config = new DocumentBuilder().setTitle('Mega Vitt API').setVersion('0.1').build();
+  const document = SwaggerModule.createDocument(app, config);
 
   app.useGlobalPipes(new ZodValidationPipe()).enableCors();
-  const PORT = process.env.APP_PORT || 3001;
-
-  const config = new DocumentBuilder().setTitle('Mega Vitt API').setVersion('0.1').build();
-
-  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(PORT, () => {
-    console.log(`SWAGGER AVAILABLE ON http://localhost:${PORT}/api/docs/`);
-  });
+  await app.listen(PORT, () => console.log(`SWAGGER AVAILABLE ON http://localhost:${PORT}/api/docs/`));
 }
 bootstrap();
