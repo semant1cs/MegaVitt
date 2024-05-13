6d14c48da411ca6e04f523aa1b78c4cc27633477
RandoSky <ovinkin.kir@gmail.com>
2024-05-14 02:46:30 +0500

    Сделал нормальную логику добавления элементов в дом дерево

--- frontend/src/components/Creator/Test/index.tsx
+++ frontend/src/components/Creator/Test/index.tsx
@@ -1,25 +1,20 @@
-import { type FC, type ReactElement, useState, useEffect } from "react";
+import { FC, useState, useMemo, ReactElement, useEffect } from "react";
 import styles from "./Test.module.scss";
 import LayoutBody from "@layout/Body";
 import { TCreatorViewProps } from "../Creator.types";
 import { randomId } from "../../../utils/getRandomId";
-import { type TNode, virtualDOM } from "../../../utils/renderVirualDOM";
+import { observer } from "mobx-react-lite";
+import VirtualDomStore, { type TNode } from "@store/VirtualDomStore";
+import HTMLReactParser from "html-react-parser/lib/index";
+import { toJS } from "mobx";
 
-const CreatorView: FC<TCreatorViewProps> = () => {
-  const [vDom, setVDom] = useState<TNode>();
-  const [dom, setDom] = useState<ReactElement | null>();
-
-  const [draggableNode, setDraggableNode] = useState<HTMLElement>();
-  const [droppableNode, setDroppableNode] = useState<HTMLElement>();
-
+const CreatorView: FC<TCreatorViewProps> = observer(() => {
   function handleOnDragStart(event: React.DragEvent<HTMLElement>) {
-    setDraggableNode(event.currentTarget);
-    // console.log("event start", event);
+    event.dataTransfer.setData("draggableNode", event.currentTarget.id);
   }
 
   function handleOnDragOver(event: React.DragEvent<HTMLElement>) {
     event.preventDefault();
-    // console.log("event over", event);
   }
 
   function handleOnDragEnter(event: React.DragEvent<HTMLElement>) {
@@ -32,55 +27,61 @@ const CreatorView: FC<TCreatorViewProps> = () => {
 
   function handleOnDragEnd(event: React.DragEvent<HTMLElement>) {
     event.preventDefault();
-    event.currentTarget.style.border = "1px solid white";
-
+    event.stopPropagation();
+    
     const droppableNodeTarget = event.currentTarget;
-    setDroppableNode(droppableNodeTarget);
+    const draggableNodeId = event.dataTransfer.getData("draggableNode");
+    const draggableNodeTarget = document.getElementById(draggableNodeId);
 
-    // console.log("draggableNode", draggableNode);
-    if (!droppableNodeTarget || !draggableNode) return;
+    droppableNodeTarget.style.border = "1px solid white";
 
-    // console.log("droppableNode", droppableNodeTarget);
-    // console.log("draggableNode", draggableNode);
+    if (!droppableNodeTarget || !draggableNodeTarget) return;
 
     const droppableProps: TNode["props"] = {
-      ...virtualDOM.getAllAttributes(droppableNodeTarget),
+      ...VirtualDomStore.getAllAttributes(droppableNodeTarget),
       className: droppableNodeTarget.className,
-      draggable: false,
     };
 
     const draggableProps: TNode["props"] = {
-      ...virtualDOM.getAllAttributes(draggableNode),
-      className: draggableNode.className,
+      ...VirtualDomStore.getAllAttributes(draggableNodeTarget),
+      key: randomId(),
       draggable: false,
+      onDrop: handleOnDragEnd,
+      onDragOver: handleOnDragOver,
+      onDragEnter: handleOnDragEnter,
+      onDragLeave: handleOnDragLeave,
+      className: draggableNodeTarget.className,
     };
 
     delete droppableProps.class;
     delete draggableProps.class;
 
-    const virtualDroppable = virtualDOM.createVNode(
-      droppableNodeTarget?.tagName.toLowerCase() || "div",
-      droppableProps,
-      [],
+    const virtualDraggable = VirtualDomStore.createVNode(
+      draggableNodeTarget.tagName.toLowerCase(),
+      draggableProps,
+      draggableNodeTarget.childNodes,
     );
-    const virtualDraggable = virtualDOM.createVNode(draggableNode?.tagName.toLowerCase() || "div", draggableProps, []);
 
-    const newVDom = virtualDOM.appendChild(virtualDroppable, virtualDraggable);
-
-    setVDom(prev => {
-      return prev ? { ...prev, children: [newVDom] } : newVDom;
-    });
-    // setVDom(prev => {
-    //   return prev ? { ...prev, children: [...prev.children, virtualDraggable] } : virtualDraggable;
-    // });
+    console.log(droppableProps);
+    VirtualDomStore.appendChild(droppableProps, virtualDraggable);
   }
 
   useEffect(() => {
-    if (!vDom) return;
-
-    // console.log(vDom);
-    setDom(virtualDOM.createDOMNode(vDom));
-  }, [vDom]);
+    if (!VirtualDomStore.vDom) {
+      VirtualDomStore.updateVDom({
+        tagName: "div",
+        props: {
+          id: "app",
+          onDrop: handleOnDragEnd,
+          className: styles.canvas__app,
+          onDragOver: handleOnDragOver,
+          onDragEnter: handleOnDragEnter,
+          onDragLeave: handleOnDragLeave,
+        },
+        children: [],
+      });
+    }
+  }, []);
 
   return (
     <LayoutBody classNames={{ body__container: styles.creator }}>
@@ -89,7 +90,6 @@ const CreatorView: FC<TCreatorViewProps> = () => {
         <ul>
           <div
             id={randomId()}
-            key="text"
             draggable="true"
             className={styles.text}
             onDragStart={handleOnDragStart}
@@ -98,7 +98,6 @@ const CreatorView: FC<TCreatorViewProps> = () => {
           </div>
           <div
             id={randomId()}
-            key="flex"
             draggable="true"
             className={styles.flex__item}
             onDragStart={handleOnDragStart}
@@ -107,7 +106,6 @@ const CreatorView: FC<TCreatorViewProps> = () => {
           </div>
           <div
             id={randomId()}
-            key="flex-container"
             draggable="true"
             className={styles.flex__container}
             onDragStart={handleOnDragStart}
@@ -118,19 +116,10 @@ const CreatorView: FC<TCreatorViewProps> = () => {
       </div>
       <div>
         <h2>Canvas</h2>
-        <div
-          id="app"
-          className={styles.canvas}
-          onDragEnter={handleOnDragEnter}
-          onDragLeave={handleOnDragLeave}
-          onDrop={handleOnDragEnd}
-          onDragOver={handleOnDragOver}
-        >
-          {dom}
-        </div>
+        <div className={styles.canvas}>{VirtualDomStore.dom}</div>
       </div>
     </LayoutBody>
   );
-};
+});
 
 export default CreatorView;
